0a57e97b48499ba61f7870d381296eb6cb7eb7cd
Michael Luo <michael.luo0801@gmail.com>
2018-08-09 20:30:45 +1000

    add prettier

--- server/src/app.js
+++ server/src/app.js
@@ -9,6 +9,7 @@ const config = require('./config/Config');
 const passport = require('passport');
 const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
+require('./config/passport')(passport);
 
 const app = express();
 const router = express.Router();
@@ -16,57 +17,62 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(session({
-  secret: config.SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { httpOnly: false }
-}))
+app.use(
+  session({
+    secret: config.SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { httpOnly: false }
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 //connect to mongodb
-mongoose.connect(config.DB, function() {
-  console.log('Connection has been made');
-})
-.catch(err => {
-  console.error('App starting error:', err.stack);
-  process.exit(1);
-});
+mongoose
+  .connect(
+    config.DB,
+    function() {
+      console.log('Connection has been made');
+    }
+  )
+  .catch(err => {
+    console.error('App starting error:', err.stack);
+    process.exit(1);
+  });
 
 // Include controllers
-fs.readdirSync("controllers").forEach(function (file) {
-  if(file.substr(-3) == '.js') {
+fs.readdirSync('src/controllers').forEach(function(file) {
+  if (file.substr(-3) == '.js') {
     const route = require('./controllers/' + file);
     route.controller(app);
   }
-})
+});
 app.use(history());
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + '/dist'));
 
 router.get('/api/current_user', isLoggedIn, function(req, res) {
-  if(req.user) {
-    res.send({ current_user: req.user })
+  if (req.user) {
+    res.send({ current_user: req.user });
   } else {
     res.status(403).send({ success: false, msg: 'Unauthorized.' });
   }
-})
+});
 
 function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
+  if (req.isAuthenticated()) return next();
 
   res.redirect('/');
-  console.log('error! auth failed')
+  console.log('error! auth failed');
 }
 
-router.get('/api/logout', function(req, res){
+router.get('/api/logout', function(req, res) {
   req.logout();
   res.send();
 });
 
 router.get('/', function(req, res) {
-  res.json({ message: 'API Initialized!'});
+  res.json({ message: 'API Initialized!' });
 });
 
 const port = process.env.API_PORT || 8081;
